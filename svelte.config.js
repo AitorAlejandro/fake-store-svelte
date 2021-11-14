399f982094fbb2d4b3f80eabd98f12dc0761ff52
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-11-14 22:14:09 +0100

    add adapter-static dep

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	adapter: adapter(),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
