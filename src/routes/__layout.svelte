26a95d2b42cffe3c85dd1e016e65a3ffb7afa61f
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-11-18 23:12:30 +0100

    add normalize and milligram by CDN

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,19 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+    <!-- CSS Reset -->
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
+    />
+    <!-- Milligram CSS -->
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css"
+    />
+</svelte:head>
+
 <header style={{ backgroundColor: '#eaeaea' }}>
 	<h1>Fake Store Home</h1>
 </header>
