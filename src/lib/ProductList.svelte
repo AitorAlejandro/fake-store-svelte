0bc7fe8749ac1bf500a2cf7ebc6fa30693f8a7ae
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-11-21 22:51:48 +0100

    display products

--- src/lib/ProductList.svelte
+++ src/lib/ProductList.svelte
@@ -1,3 +1,30 @@
-<div>
-	this is the ProductList
+<script>
+	import { onMount } from 'svelte';
+	import axios from 'axios';
+
+	let products = [];
+
+	onMount(async function () {
+		try {
+			const response = await axios.get('https://fakestoreapi.com/products');
+			products = response.data;
+		} catch (error) {
+			console.error(error);
+		}
+	});
+</script>
+
+<div class="container">
+	<ul>
+		{#each products as product}
+			<li>
+				<div>{product.id}</div>
+				<div>{product.title}</div>
+				<div>{product.price}</div>
+				<div>{product.description}</div>
+				<div>{product.category}</div>
+				<img alt="" src={product.image} />
+			</li>
+		{/each}
+	</ul>
 </div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -15,14 +15,14 @@
     />
 </svelte:head>
 
-<header style={{ backgroundColor: '#eaeaea' }}>
+<header>
 	<h1>Fake Store Home</h1>
 </header>
 <main>
 	<slot />
 </main>
-<footer style={{ backgroundColor: '#eaeaea' }}>
-	<h2>Store Footer</h2>
+<footer>
+	{new Date().getFullYear()} &copy; Fake Store Api
 </footer>
 
 <style>
